7d1f9aa982eec4b06139850530d72b137d3d604e
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-13 13:56:10 +0530

    Update provisiones-asscoadas.service.ts

--- src/app/adif-features/provisiones-asscoadas/service/provisiones-asscoadas.service.ts
+++ src/app/adif-features/provisiones-asscoadas/service/provisiones-asscoadas.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../../environments/environment.prod'; 
 import { Observable } from 'rxjs/internal/Observable';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/internal/operators/map';
