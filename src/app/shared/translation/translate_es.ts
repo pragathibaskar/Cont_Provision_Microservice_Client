131a9c18f05a81fa58a20ea8f36c0e3c63784a19
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-24 16:39:36 +0530

    added serviceUnAvailable msg

--- src/app/shared/translation/translate_es.ts
+++ src/app/shared/translation/translate_es.ts
@@ -4,13 +4,15 @@ export class TranslationES {
         update: { action: 'success', msg: 'actualizado exitosamente' },
         error: { action: 'error', msg: 'el registro ya existe' },
         delete: { action: 'success', msg: 'borrado exitosamente' },
-        sorryForIncovinience: { action: 'error', msg: 'lo siento por incoviencia' }
+        sorryForIncovinience: { action: 'error', msg: 'lo siento por incoviencia' },
+       serviceUnAvailable: { action: 'error', msg: 'El servicio no está disponible. Por favor, inténtelo más tarde' }
     };
     static provisiones_contable = {
         success: { action: 'success', msg: 'creado con éxito Provisiones' },
         update: { action: 'success', msg: 'actualizado exitosamente' },
         error: { action: 'error', msg: 'el registro ya existe' },
         delete: { action: 'success', msg: 'borrado exitosamente' },
-        sorryForIncovinience: { action: 'error', msg: 'lo siento por incoviencia' }
+        sorryForIncovinience: { action: 'error', msg: 'lo siento por incoviencia' },
+        serviceUnAvailable: { action: 'error', msg: 'El servicio no está disponible. Por favor, inténtelo más tarde' }
     };
 }
